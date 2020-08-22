6813cb00b5016f803403e352f4e0b55dca25e73f
Vaughub <olakamfjord@gmail.com>
2020-08-22 15:29:08 +0200

    upload

---
+++ Players/Players/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace Players
+{
+	class Player
+	{
+		private string Name;
+		private int Points;
+
+		public Player(string name, int points)
+		{
+			Name = name;
+			Points = points;
+		}
+
+		public void Play(Player player2)
+		{
+			Random random = new Random();
+			while (true)
+			{
+				int power1 = random.Next(100);
+				int power2 = random.Next(100);
+				if (power1 > power2)
+				{
+					Points++;
+					player2.Points--;
+					break;
+				}
+				if (power1 < power2)
+				{
+					Points--;
+					player2.Points++;
+					break;
+				}
+			}
+		}
+
+		public void ShowNameAndPoints()
+		{
+			Console.WriteLine(Name + " - " + Points);
+		}
+	}
+}
