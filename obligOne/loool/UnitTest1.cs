ed73e42468dbbb86c9a0f547b698ffcd8b5b6808
Vaughub <olakamfjord@gmail.com>
2020-08-26 22:06:50 +0200

    upload

---
+++ obligOne/loool/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace loool
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ obligOne/obligOne/FamilyApp.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace obligOne
+{
+	public class FamilyApp
+	{
+		public Person[] Family { get; }
+		public readonly string WelcomeMessage = "Velkommen bruker";
+		public readonly string CommandPrompt = "Kommando: ";
+
+		public FamilyApp(params Person[] kek)
+		{
+			Family = kek;
+		}
+
+		public string HandleCommand(string line)
+		{
+			if (!Regex.IsMatch(line, "(^(hjelp|liste)$|^(vis \\d+)$)")) 
+				return "Ugyldig kommando ('hjelp' for å se alle kommandoene)";
+
+			string[] arr = line.Split(" ");
+			string command = arr[0];
+			if (command == "hjelp") return WriteHelpText();
+			if (command == "liste") return ShowEveryoneInList();
+			return command == "vis" ? ShowPersonWithId(arr[1]) : "";
+		}
+
+		private string ShowPersonWithId(string idNr)
+		{
+			int number = int.Parse(idNr);
+			var main = new StringBuilder();
+			var children = new StringBuilder();
+			foreach (var person in Family)
+			{
+				if (person.Id == number) main.Append(person.GetDescription() + "\n");
+				if (person.Mother == null && person.Father == null) continue;
+				if ((person.Father?.Id ?? person.Mother.Id) == number) children.Append(person.GetChildren() + "\n");
+			}
+
+			if (main.Length == 0) return "Person ikke funnet";
+			var childTag = children.Length > 0 ? "  Barn:\n" : "";
+
+			return main + childTag + children;
+		}
+
+		private string ShowEveryoneInList()
+		{
+			var tempFamily = new StringBuilder();
+			foreach (var person in Family)
+			{
+				tempFamily.AppendLine(person.GetDescription());
+			}
+
+			return tempFamily.ToString();
+		}
+
+		private string WriteHelpText()
+		{
+			return   "hjelp     Viser en hjelpetekst som forklarer alle kommandoene\n"
+			       + "liste     Lister alle personer med id, fornavn, fødselsår, dødsår og navn og id på mor og far om det finnes registrert\n"
+			       + "vis <id>  Viser en bestemt person med mor, far og barn (og id for disse, slik at man lett kan vise en av dem)\n";
+		}
+	}
+}
