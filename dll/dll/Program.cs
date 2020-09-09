413bbf1b44a54a1b432dd1aa4d10af2b869443f6
Vaughub <olakamfjord@gmail.com>
2020-09-09 19:25:35 +0200

    upload

---
+++ dll/dll/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PomodoroEngine;
+
+namespace dll
+{
+	class Program : INotifyObject
+	{
+		static void Main(string[] args)
+		{
+			var program = new Program();
+			Console.ReadLine();
+		}
+
+		public Program()
+		{
+			var pomodoro = new Pomodoro(this, 1);
+			pomodoro.Start();
+		}
+
+		public void Tick(int minutes, int seconds)
+		{
+			Console.Clear();
+			if (minutes == 0 && seconds == 0)
+			{
+				Console.WriteLine("Take a break");
+				Environment.Exit(0);
+			}
+			Console.WriteLine($"{minutes}:{seconds}");
+		}
+	}
+}
